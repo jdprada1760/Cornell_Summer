f6a58e397f1398e754381b40621ad469e0fd85c5
jdprada1760 <jd.prada1760@uniandes.edu.co>
2015-06-15 11:40:24 -0400

    Histograms for masses and Luminosities

---
+++ Millenium_Run/galaxies.sql
@@ -0,0 +1,75 @@
+ --Position of a corner of the box  
+DECLARE @posx FLOAT  
+DECLARE @posy FLOAT  
+DECLARE @posz FLOAT
+--Snapshot to be analized
+DECLARE @snapnum INT
+--Scale to generate random number  
+DECLARE @l FLOAT  
+--Size of the box  
+DECLARE @bsize FLOAT  
+--Minimum and Maximum logarithms of Masses and Luminosities
+DECLARE @minM FLOAT
+DECLARE @maxM FLOAT
+DECLARE @minL FLOAT
+DECLARE @maxL FLOAT
+--Number of bins in the histogram
+DECLARE @nbins INT
+--Mass (in solar masses) per particle
+DECLARE @mp FLOAT
+--Solar luminosity (erg/s)
+DECLARE @lsun FLOAT
+--Solar visual absolute magnitude
+DECLARE @sunvm FLOAT
+
+--Assignment of values
+SET @sunvm = 
+SET @lsun = 3.846E+33
+SET @mp = 8.61E+8
+SET @snapnum = 63
+SET @nbins = 10
+SET @l = 0  
+SET @bsize = 10  
+SELECT @posx = @l*RAND()  
+SELECT @posy = @l*RAND()  
+SELECT @posz = @l*RAND()  
+
+--Selects the minimum and maximum masses
+SELECT @minM = MIN(LOG(@mp*D.np)),
+       @maxM = MAX(LOG(@mp*D.np)),
+       @minL = MIN((@sunmv - D.mag_v)/2.5)
+       @maxL = MAX((@sunmv - D.mag_v)/2.5)
+FROM MPAHalo D
+WHERE D.x > @posx AND D.x < @posx + @bsize  
+  AND D.y > @posx AND D.y < @posy + @bsize  
+  AND D.z > @posx AND D.z < @posz + @bsize
+  AND D.snapnum = @snapnum
+
+--Defines the interval of the histogram for masses and luminosities
+DECLARE @intervm FLOAT
+DECLARE @intervL FLOAT
+SET @intervm = ( @maxM - @minM ) / @nbins
+SET @intervL = ( @maxL - @minL ) / @nbins
+
+--Selects the histogram logarithmic scale for masses
+SELECT @intervm*(FLOOR((LOG(D.np*@mp)-@minM)/@intervm))+@minM AS logM,
+       COUNT(*) AS NUM
+FROM MPAHalo D
+WHERE D.x > @posx AND D.x < @posx + @bsize  
+  AND D.y > @posx AND D.y < @posy + @bsize  
+  AND D.z > @posx AND D.z < @posz + @bsize
+  AND D.snapnum = @snapnum
+GROUP BY @intervm*(FLOOR((LOG(D.np*@mp)-@minM)/@intervm))+@minM
+ORDER BY logM
+
+--Selects the histogram logarithmic scale for luminosities
+
+SELECT @intervL*(FLOOR((((@sunmv - D.mag_v)/2.5)-@minL)/@interv))+@minL AS logL,
+       COUNT(*) AS NUM
+FROM MPAHalo D
+WHERE D.x > @posx AND D.x < @posx + @bsize  
+  AND D.y > @posx AND D.y < @posy + @bsize  
+  AND D.z > @posx AND D.z < @posz + @bsize
+  AND D.snapnum = @snapnum
+GROUP BY @intervL*(FLOOR((((@sunmv - D.mag_v)/2.5)-@minL)/@interv))+@minL
+ORDER BY logL
