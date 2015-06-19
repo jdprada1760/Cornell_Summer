0eeccb1e11e1584c73a4c923b3b981e51e68bf2f
jdprada1760 <jd.prada1760@uniandes.edu.co>
2015-06-19 10:49:46 -0400

    zzz

--- Millenium_Run/g_environment.sql
+++ Millenium_Run/g_environment.sql
@@ -10,6 +10,10 @@ DECLARE @hz FLOAT
 DECLARE @vx FLOAT
 DECLARE @vy FLOAT
 DECLARE @vz FLOAT
+--Normalized vector from pov to halo
+DECLARE @dx FLOAT
+DECLARE @dy FLOAT
+DECLARE @dz FLOAT
 --Snapshot to be analized
 DECLARE @snapnum INT
 --Hubble constant
@@ -19,6 +23,7 @@ DECLARE @H FLOAT
 SET @x = 0
 SET @y = 0
 SET @z = 0
+SET @H = 1
 SET @id = 1234
 SELECT @hx = M.x,
        @hy = M.y,
@@ -28,7 +33,28 @@ SELECT @hx = M.x,
        @vz = M.velZ
 FROM MPAHalo M
 WHERE M.haloID = @id
+SET @dx = (@hx - @x)/SQRT( POWER(@hx - @x,2) + POWER(@hx - @x,2) + POWER(@hx - @x,2) )
+SET @dy = (@hy - @y)/SQRT( POWER(@hx - @x,2) + POWER(@hx - @x,2) + POWER(@hx - @x,2) )
+SET @dz = (@hz - @z)/SQRT( POWER(@hx - @x,2) + POWER(@hx - @x,2) + POWER(@hx - @x,2) )
 
---Selecciona el primer neighbour del halo
 
-WHERE D.
+
+
+--Makes an ordered list distances to galaxies
+SELECT SQRT(
+       POWER(  ((D.x - @x)*@dx + (D.y - @y)*@dy + (D.z - @z)*@dz)*@dx - (D.x - @x)  , 2 )  +
+       POWER(  ((D.x - @x)*@dx + (D.y - @y)*@dy + (D.z - @z)*@dz)*@dy - (D.y - @y)  , 2 )  +
+       POWER(  ((D.x - @x)*@dx + (D.y - @y)*@dy + (D.z - @z)*@dz)*@dz - (D.z - @z)  , 2 )  +
+       ) AS DIST
+FROM DeLucia2006a D
+WHERE --Omits galaxies with r abs magnitude < -19
+      D.mag_r < -19
+      --Only counts galxies with relative velocities to the halo < 500 Km/s
+      AND ABS(  
+      	       --Radial component of the total velocity of the galaxy
+	       @dx*(@H*D.x + @velX) + @dy*(@H*D.y + @velY) + @dz*(@H*D.z + @velZ)
+	       --Radial component of the total velocity of the halo
+	       -@dx*(@H*@hx + @vx) + @dy*(@H*@hy + @vy) + @dz*(@H*@hz + @vz)
+
+	     ) < 500
+ORDER BY DIST
