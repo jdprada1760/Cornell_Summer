84795c2d5d0d70eb5bcde037abeb91b91f5f6b1f
jdprada1760 <jd.prada1760@uniandes.edu.co>
2015-06-23 13:59:14 -0400

    Calculating environment for multiple haloes

---
+++ Millenium_Run/multi_env.sql
@@ -0,0 +1,22 @@
+--Position of the point of view
+DECLARE @x FLOAT
+DECLARE @y FLOAT
+DECLARE @z FLOAT
+--Snapshot to be analized
+DECLARE @snapnum INT
+--Hubble constant
+DECLARE @H FLOAT
+--Neighbour
+DECLARE @neigh INT
+
+--Asignment of variables
+SET @snapnum = 63
+SET @neigh = 4
+SET @x = 250
+SET @y = 250
+SET @z = 250
+SET @H = 100
+
+
+
+
