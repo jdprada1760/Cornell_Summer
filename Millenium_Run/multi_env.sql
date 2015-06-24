899997168f6c92bbb3656efcbb65da664558bd1c
jdprada1760 <jd.prada1760@uniandes.edu.co>
2015-06-23 22:03:25 -0400

    algunos avances

--- Millenium_Run/multi_env.sql
+++ Millenium_Run/multi_env.sql
@@ -8,54 +8,80 @@ DECLARE @snapnum INT
 DECLARE @H FLOAT
 --Neighbour
 DECLARE @neigh INT
+--Limits the box of halos
+DECLARE @limx FLOAT
+DECLARE @limy FLOAT
+DECLARE @limz FLOAT
+DECLARE @limMx FLOAT
+DECLARE @limMy FLOAT
+DECLARE @limMz FLOAT
+
 
 --Asignment of variables
 SET @snapnum = 63
 SET @neigh = 4
-SET @x = 250
-SET @y = 250
-SET @z = 250
+SET @x = 0
+SET @y = 0
+SET @z = 0
+SET @limx = 40
+SET @limy = 40
+SET @limz = 40
+SET @limMx = 60
+SET @limMy = 60
+SET @limMz = 60
 SET @H = 100
 
-            (SELECT SQRT
-                   POWER(  ((c.gx - @x)*c.dx + (c.gy - @y)*c.dy + (c.gz - @z)*c.dz)*c.dx - (c.gx - @x)  , 2 )  +
-                   POWER(  ((c.gx - @x)*c.dx + (c.gy - @y)*c.dy + (c.gz - @z)*c.dz)*c.dy - (c.gy - @y)  , 2 )  +
-                   POWER(  ((c.gx - @x)*c.dx + (c.gy - @y)*c.dy + (c.gz - @z)*c.dz)*c.dz - (c.gz - @z)  , 2 )
-                              ) AS DIST,
-             FROM
-                  -- Selects the position, velocities and position, velocities of galaxies and haloes and the unit vectors to haloes
-                  (SELECT D.x AS gx,
-                          D.y AS gy,
-                          D.z AS gz,
-                          M.x AS hx,
-                          M.y AS hy,
-                          M.z AS hz,
-                          D.velX AS gvelX,
-                          D.velY AS gvelY,
-                          D.velZ AS gvelZ,
-                          M.velX AS hvelX,
-                          M.velY AS hvelY,
-                          M.velZ AS hvelZ,
-                          (M.x - @x)/SQRT( POWER(M.x - @x,2) + POWER(M.y - @y,2) + POWER(M.z - @z,2) ) AS dx,
-                          (M.y - @y)/SQRT( POWER(M.x - @x,2) + POWER(M.y - @y,2) + POWER(M.z - @z,2) ) AS dy,
-                          (M.z - @z)/SQRT( POWER(M.x - @x,2) + POWER(M.y - @y,2) + POWER(M.z - @z,2) ) AS dz,
-                   FROM DeLucia2006a D,
-                        MPAHalo M
-                   WHERE --Selects snapshot
-                         D.snapnum = @snapnum
-                         AND M.snapNum = @snapnum
-                         --Omits galaxies with r abs magnitude < -19
-                         AND D.mag_r < -19
-                  )
+DECLARE @hid INT
+DECLARE @n INT
+SET @hid = 1;
+SET @n = 1;
 
+SELECT DIST,
+       @n := if(@hid = haloID, @n + 1, 1) AS row_number,
+       @hid := haloID AS dummy
+FROM
+      (SELECT TOP 10000000 SQRT(
+            POWER(  ((c.gx - @x)*c.dx + (c.gy - @y)*c.dy + (c.gz - @z)*c.dz)*c.dx - (c.gx - @x)  , 2 )  +
+            POWER(  ((c.gx - @x)*c.dx + (c.gy - @y)*c.dy + (c.gz - @z)*c.dz)*c.dy - (c.gy - @y)  , 2 )  +
+            POWER(  ((c.gx - @x)*c.dx + (c.gy - @y)*c.dy + (c.gz - @z)*c.dz)*c.dz - (c.gz - @z)  , 2 )
+                ) AS DIST,
+            c.haloID AS haloID
+      FROM
+          -- Selects the position, velocities and position, velocities of galaxies and haloes and the unit vectors to haloes
+          (SELECT M.haloID AS haloID,
+                  D.x AS gx,
+                  D.y AS gy,
+                  D.z AS gz,
+                  M.x AS hx,
+                  M.y AS hy,
+                  M.z AS hz,
+                  --Difference in peculiar velocities
+                  D.velX - M.velX AS dvx,
+                  D.velY - M.velY AS dvy,
+                  D.velZ - M.velZ AS dvz,
+                  (M.x - @x)/SQRT( POWER(M.x - @x,2) + POWER(M.y - @y,2) + POWER(M.z - @z,2) ) AS dx,
+                  (M.y - @y)/SQRT( POWER(M.x - @x,2) + POWER(M.y - @y,2) + POWER(M.z - @z,2) ) AS dy,
+                  (M.z - @z)/SQRT( POWER(M.x - @x,2) + POWER(M.y - @y,2) + POWER(M.z - @z,2) ) AS dz
+           FROM DeLucia2006a D,
+                MPAHalo M
+           WHERE --Selects snapshot
+                 D.snapnum = @snapnum
+                 AND M.snapNum = @snapnum
+                 -- Limits the box of haloes
+                 AND D.x > @limx AND  D.y > @limy AND D.z > @limz
+                 AND D.x < @limMx AND  D.y < @limMy AND D.z < @limMz
+                 --Omits galaxies with r abs magnitude < -19
+                 AND D.mag_r < -19
+          ) c
+      WHERE --Only counts galxies with relative radial velocities to the halo < 500 Km/s
+           ABS(
+               -- Radial component of the difference in peculiar velocities
+               c.dvx*c.dx + c.dvy*c.dy + c.dvz*c.dz +
+               -- Radial component of the diference in velocities due to Hubbles law
+               @H*((c.gx - c.hx)*dx + (c.gy - c.hy)*dy + (c.gy - c.hy)*dy)
 
-             WHERE
-                  --Only counts galxies with relative velocities to the halo < 500 Km/s
-                  ABS(
-                  --Radial component of the total velocity of the galaxy
-                      c.dx*(@H*(c.gx-@x) + c.gvelX) + c.dy*(@H*(c.gy-@y) + c.gvelY) + c.dz*(@H*(c.gz-@z) + c.gvelZ)
-                  --Radial component of the total velocity of the halo
-                      -(c.dx*(@H*(c.hx-@x) + c.hvelX) + c.dy*(@H*(c.hy-@y) + c.hvelY) + c.dz*(@H*(c.hz-@z) + c.hvelZ))
+             )  < 500
 
-                     ) < 500
-             ) a
+      ORDER BY haloID, DIST
+    ) f
+HAVING row_number = @neigh
