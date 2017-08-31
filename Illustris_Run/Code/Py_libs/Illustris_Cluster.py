6a0d3562d5c759e142e32225f89291f48d756cb2
Jesus Prada <jd.prada1760@uniandes.edu.co>
2017-08-31 04:23:38 +0200

    Parallelized environment script

--- Illustris_Run/Code/Py_libs/Illustris_Cluster.py
+++ Illustris_Run/Code/Py_libs/Illustris_Cluster.py
@@ -1,3 +1,9 @@
+# This script reads Illustris1,2 &3 to produce files containing phaseSpace as well as the masses info of subhalos and 
+# Phase space info is kept in ..../Illustris_Run/data/phaseSpace/Illustris{1..3}.csv
+# Masses are kept in ..../Illustris_Run/data/Galaxy{1..3}.csv
+# Phase space format is:     x,y,z,vx,vy,vz,mG,mDM
+# Masses format is:    mG,mDM,m_,m_,mStellar,mBH
+
 import numpy as np
 from arepo import *
 
@@ -5,29 +11,33 @@ from arepo import *
 path1 = "/hits/universe/Illustris/L75n1820C/" 
 path2 = "/hits/universe/Illustris/L75n910FP/" 
 path3 = "/hits/universe/Illustris/L75n455FP/"
-# Filenames
+# Filenames for phaseSpace
 filenames = ['Illustris1','Illustris2','Illustris3']
+massFilenames = ['Galaxy1','Galaxy2','Galaxy3']
 # Snapshot number
 snapnum = 135
-for path,filename in zip([path1,path2,path3],filenames): 
+for path,filename,massfn in zip([path1,path2,path3],filenames,massFilenames): 
  
     # Loads groups to select the principal halo
-    subSn = Subfind(path,snapnum)
+    subSn = Subfind(path,snapnum,combineFiles=True, verbose = True)
     
     # Loads important data
-    pos  = subSn.subhalo.SubhaloPos
-    vel  = subSn.subhalo.SubhaloVel
-    mass = subSn.subhalo.SubhaloMassType
+    pos  = subSn.SubhaloPos
+    vel  = subSn.SubhaloVel
+    mass = subSn.SubhaloMassType
 
     # Have into account only subhalos with mass
-    ind, = np.where(mass[:,0]>0)
+    ind, = np.where(mass.T[0]>0)
+    print(len(pos))
     pos  = pos[ind]
     vel  = vel[ind]
     mass = mass[ind]
 
     # Creates and fills the numpy array to save
-    arr = np.zeros((len(pos),12))
+    arr = np.zeros((len(pos),8),dtype = np.float)
     arr[:,:3]  = pos
     arr[:,3:6] = vel
-    arr[:,6:]  = mass
-    np.savetxt("../data/phaseSpace/"+filename+".txt",arr)
+    arr[:,6:]  = mass[:,:2]
+    np.savetxt("../data/phaseSpace/"+filename+".csv",arr,delimiter = ',')
+    np.savetxt("../data/"+massfn+".csv",mass,delimiter = ',')
+    
